f41e8eb436834c89563836926ca7722d3bc5de61
jack <jack.yuan@yeaoh.net>
2015-10-27 21:14:36 +0800

    update code

--- sites/all/modules/pingan_login/js/pingan_login.js
+++ sites/all/modules/pingan_login/js/pingan_login.js
@@ -2,18 +2,21 @@ jQuery(window).load(function(){
 	jQuery(".popRegister").click(function(){
 		jQuery('.titleRegister').show();
 		jQuery('.titleLogin').hide();
-		
+		jQuery("body").css("overflow","hidden");
 		jQuery(this).parent().find(".popSrc").attr('src',"/pingan_login/mobile/register");
 		jQuery(this).parent().find(".loginBody").show();
+		
 	});
 	jQuery(".popLogin").click(function(){
 		jQuery('.titleRegister').hide();
 		jQuery('.titleLogin').show();
-		
+		jQuery("body").css("overflow","hidden");
 		jQuery(this).parent().find(".popSrc").attr('src',"/pingan_login/mobile/login");
 		jQuery(this).parent().find(".loginBody").show();
+		
 	});
 	jQuery(".popClose").click(function(){
+		jQuery("body").css("overflow","auto");
 		jQuery(this).parent().parent(".loginBody").hide();
 	});
 })
