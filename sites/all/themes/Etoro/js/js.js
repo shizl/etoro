e21b6cc01282d585502f9a56a8167770ed53e4ef
yeaoh12 <zhong.liang@yeaot.net>
2015-10-30 14:31:38 +0800

    update style

--- sites/all/themes/Etoro/js/js.js
+++ sites/all/themes/Etoro/js/js.js
@@ -87,10 +87,10 @@ if(jQuery("body").width()>1150){
 
     if(jQuery(this).parent().find('.views-field-description').is(':visible')){
 
- 	jQuery(this).css({'background-image':'url("/sites/all/themes/Etoro/images/shang1.png")','background-position': '98% center','background-repeat': 'no-repeat'});
+ 	jQuery(this).css({'background-image':'url("/sites/all/themes/Etoro/images/shang1.png")','background-position': '99% center','background-repeat': 'no-repeat'});
    
    }else{
- 	jQuery(this).css({'background-image':'url("/sites/all/themes/Etoro/images/xia1.png")','background-position':'98% center','background-repeat':'no-repeat'});
+ 	jQuery(this).css({'background-image':'url("/sites/all/themes/Etoro/images/xia1.png")','background-position':'99% center','background-repeat':'no-repeat'});
    }
 
 
