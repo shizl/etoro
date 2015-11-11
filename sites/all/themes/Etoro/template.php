b414eeb348a871f5e6ac3c590bcae2e804b2b234
Your Name <you@example.com>
2015-11-11 17:18:05 +0800

    task#57

--- sites/all/themes/Etoro/template.php
+++ sites/all/themes/Etoro/template.php
@@ -159,7 +159,7 @@ function etoro_field__taxonomy_term_reference($variables) {
           
          if($form['#id'] == 'registration-form'){
             global $language;
-           if ($language->language == "zh-hans"){
+           if ($language->prefix == "cn"){
            $form['state']['#options']['complete'] = '完成';
                }else{
                 return;
